1bcb9b243655b62ca6406b89baab5d7400e45d05
peterdavies222 <peterdavies222@gmail.com>
2025-02-11 17:40:52 +0800

    update

--- main.js
+++ main.js
@@ -20,3 +20,7 @@ let navUl = document.querySelector('nav ul');
 let ulHeight = navUl.offsetHeight;
 
 navUl.style.top = 0.5*(navHeight + window.innerHeight - ulHeight) + 'px';
+
+/*setTimeout(() => {
+    alert("Please note that this site is currently only functional on desktop screens. If you're using a phone or tablet, it may not run as expected just yet!");
+  }, 100);*/
