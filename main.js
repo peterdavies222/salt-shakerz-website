9e53e451cecc7be2edd60829607a6b17f4b6f66e
peterdavies222 <peterdavies222@gmail.com>
2025-02-16 21:13:55 +0800

    added buttons to spotify/youtube sections

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ let navLinksHeight = navLinks.scrollHeight;
 
 window.addEventListener('load', ()=> {
     navLinksHeight = navLinks.scrollHeight;
-    console.log(navLinksHeight);
+    //console.log(navLinksHeight);
     navLinks.style.top = `-${navLinksHeight}px`;
 });
 
 window.addEventListener('resize', ()=> {
     navLinksHeight = navLinks.scrollHeight;
-    console.log(navLinksHeight);
+    //console.log(navLinksHeight);
     if(!navLinks.classList.contains('visible')){
         navLinks.style.top = `-${navLinksHeight}px`;
     };
@@ -20,7 +20,7 @@ window.addEventListener('resize', ()=> {
 
 menuButton.addEventListener('click', ()=> {
     navLinks.classList.toggle('active');
-    console.log(navLinks.classList);
+    //console.log(navLinks.classList);
 });
 
 const navLinksElements = document.querySelectorAll('nav ul li');
@@ -54,6 +54,44 @@ setTimeout(() => {
     alert("Please note that this site is currently only functional on desktop screens. If you're using a phone or tablet, it may not run as expected... yet!");
   }, 100)
 */
+
+let spotifySlideRightBtn = document.querySelector('.episodes-slide-right.spotify');
+let spotifyGallery = document.querySelector('.spotify-episodes');
+let youtubeSlideRightBtn = document.querySelector('.episodes-slide-right.youtube');
+let youtubeGallery = document.querySelector('.youtube-episodes');
+
+function checkIfScrollable(content, button){
+    if(content && content.scrollWidth > content.clientWidth) {
+        button.style.display = 'block';
+    };
+    if(content && content.scrollWidth <= content.clientWidth) {
+        button.style.display = 'none';
+    };
+};
+
+window.addEventListener('resize', ()=> {
+    checkIfScrollable(spotifyGallery, spotifySlideRightBtn);
+});
+
+window.addEventListener('load', ()=> {
+    checkIfScrollable(spotifyGallery, spotifySlideRightBtn);
+});
+
+window.addEventListener('resize', ()=> {
+    checkIfScrollable(youtubeGallery, youtubeSlideRightBtn);
+});
+
+window.addEventListener('load', ()=> {
+    checkIfScrollable(youtubeGallery, youtubeSlideRightBtn);
+});
+
+spotifySlideRightBtn.addEventListener('click', ()=> {
+    spotifyGallery.scrollLeft += 200;
+});
+
+youtubeSlideRightBtn.addEventListener('click', ()=> {
+    youtubeGallery.scrollLeft += 200;
+});
 
 const highlightsCarousel = document.querySelector('.highlights-carousel');
 const highlightsBackBtn = document.querySelector('.highlights-back');
